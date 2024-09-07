d5f89ff3ee7a0d0e5b0a6c10abc58870fedaeed0
Markéta Šlancarová <marketaslancarova@gmail.com>
2024-09-07 17:49:01 +0200

    Added products details

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import products from "../utils/products";
+
 const ShopPage = () => {
   return (
     <div>
       <h1>Shop Page</h1>
+      {products.map((product) => (
+        <>{product}</>
+      ))}
     </div>
   );
 };
---
+++ src/utils/products.js
@@ -0,0 +1,27 @@
+const products = [
+  {
+    id: 1,
+    name: "Root Chakra Bracelet",
+    description: "Supports the root chakra and promotes stability.",
+    price: 20,
+    image:
+      "https://i.pinimg.com/564x/88/77/22/88772233f21e9106dc6f43465ac8b1f2.jpg",
+  },
+  {
+    id: 2,
+    name: "Sacral Chakra Bracelet",
+    description: "Boosts creativity and emotional balance.",
+    price: 25,
+    image:
+      "https://i.pinimg.com/564x/88/77/22/88772233f21e9106dc6f43465ac8b1f2.jpg",
+  },
+  {
+    id: 3,
+    name: "Solar Plexus Chakra Bracelet",
+    description: "Increases confidence and personal power.",
+    price: 30,
+    image:
+      "https://i.pinimg.com/564x/88/77/22/88772233f21e9106dc6f43465ac8b1f2.jpg",
+  },
+];
+export default products;
